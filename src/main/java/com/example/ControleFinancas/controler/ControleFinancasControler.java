edb55c4461274d55ba8972562777e7e7aa6bb546
Vdetofa <victordetofano13@gmail.com>
2026-04-08 11:22:25 -0300

    first commit

---
+++ src/main/java/com/example/ControleFinancas/controler/ControleFinancasControler.java
@@ -0,0 +1,5 @@
+package com.example.ControleFinancas.controler;
+
+public class ControleFinancasControler {
+    
+}
---
+++ src/test/java/com/example/ControleFinancas/ControleFinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ControleFinancas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

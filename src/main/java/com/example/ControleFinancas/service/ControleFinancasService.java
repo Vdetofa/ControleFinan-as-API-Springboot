832802afd1134434fa3e2ce0c982c3e11559a252
Vdetofa <victordetofano13@gmail.com>
2026-04-10 09:58:40 -0300

    adicioando restricao de saldo

--- src/main/java/com/example/ControleFinancas/service/ControleFinancasService.java
+++ src/main/java/com/example/ControleFinancas/service/ControleFinancasService.java
@@ -15,16 +15,35 @@ public class ControleFinancasService {
     private ControleFinancasRepository repository;
 
     public void registrar(ControleFinancasModel financas){
-        if(financas.getValor()>0){
-            if (financas.getData() != null) {
-                 repository.save(financas);
-            }else{
-                financas.setData(LocalDateTime.now());
-                 repository.save(financas);
-            }
-        }else{
-            System.out.println("Erro: O valor deve ser maior que zero.");
+        // 1. Validação básica de valor
+        if (financas.getValor() <= 0) {
+            
+            System.out.println("Valor inválido");
+            return; // Sai do método e não faz mais nada
         }
+
+        // 2. DESAFIO: Coloque aqui sua validação de saldo insuficiente
+        // Dica: Use o seu método consultarSaldo() e compare com financas.getValor()
+
+        // 3. Preparação dos dados (Data)
+        if (financas.getData() == null) {
+            financas.setData(LocalDateTime.now());
+        }
+
+        if (financas.getTipo().equalsIgnoreCase("SAIDA")) {
+           double valorSaldoSaida=0.0;
+           valorSaldoSaida +=  financas.getValor();
+            consultarSaldo();
+            if (valorSaldoSaida> consultarSaldo()) {
+                System.out.print("saldo insuficiente");
+                return;
+            }
+        }
+
+        // 4. Salva uma única vez
+        repository.save(financas);
+        return ;
+                
     }
     
     public List<ControleFinancasModel> listartodos(){
